ca72c309a5520422e9974669acf8424e57a09b76
Ivy Benson <irosebenson@gmail.com>
2020-09-12 17:32:42 -0500

    added state components and updated app

--- react-playground/src/App.js
+++ react-playground/src/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
-import Messages from "./Messages";
+// import Messages from "./Messages";
 import TheDate from "./state/TheDate.js";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>YOUR APPLICATION NAME!</h1>
-        <Messages name="Messages" unread={0} />
-        <Messages name="Notifications" unread={10} />
-      </div>
-    );
+    return <TheDate />;
   }
 }
 
