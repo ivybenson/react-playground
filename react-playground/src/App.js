d3eb8b56d2423d1068f87a38148341640f37e89b
Ivy Benson <irosebenson@gmail.com>
2020-09-18 16:54:37 -0500

    updates with callback props

--- react-playground/src/App.js
+++ react-playground/src/App.js
@@ -13,6 +13,34 @@ class App extends React.Component {
     ],
   };
 
+  handleAddItem = (itemName) => {
+    const newItems = [
+      ...this.state.shoppingItems,
+      { name: itemName, checked: false },
+    ];
+    this.setState({
+      shoppingItems: newItems,
+    });
+  };
+
+  handleDeleteItem = (item) => {
+    const newItems = this.state.shoppingItems.filter((itm) => itm !== item);
+    this.setState({
+      shoppingItems: newItems,
+    });
+  };
+  handleCheckItem = (item) => {
+    const newItems = this.state.shoppingItems.map((itm) => {
+      if (itm === item) {
+        itm.checked = !itm.checked;
+      }
+      return itm;
+    });
+    this.setState({
+      shoppingItems: newItems,
+    });
+  };
+
   render() {
     return (
       <>
@@ -21,13 +49,19 @@ class App extends React.Component {
         </header>
         <main>
           <section>
-            <AddItemForm />
+            <AddItemForm onAddItem={this.handleAddItem} />
           </section>
           <section>
-            <ShoppingList items={this.state.shoppingItems} />
+            <ShoppingList
+              items={this.state.shoppingItems}
+              onDeleteItem={this.handleDeleteItem}
+              onCheckItem={this.handleCheckItem}
+            />
           </section>
         </main>
       </>
     );
   }
 }
+
+export default App;
